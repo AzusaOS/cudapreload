0fbb4898a4a5bfafa6afabf5041e7c0a12c961a5
Mark Karpeles <magicaltux@gmail.com>
2023-05-07 19:29:45 +0900

    add all libs without .1 at the end because some people use it that way

--- cudapreload.c
+++ cudapreload.c
@@ -7,17 +7,29 @@
 
 static const char *cuda_libs[] = {
 	"libcuda.so.1",
+	"libcuda.so",
 	"libnvcuvid.so.1",
+	"libnvcuvid.so",
 	"libnvidia-allocator.so.1",
+	"libnvidia-allocator.so",
 	"libnvidia-cfg.so.1",
+	"libnvidia-cfg.so",
 	"libnvidia-encode.so.1",
+	"libnvidia-encode.so",
 	"libnvidia-fbc.so.1",
+	"libnvidia-fbc.so",
 	"libnvidia-ml.so.1",
+	"libnvidia-ml.so",
 	"libnvidia-ngx.so.1",
+	"libnvidia-ngx.so",
 	"libnvidia-opencl.so.1",
+	"libnvidia-opencl.so",
 	"libnvidia-opticalflow.so.1",
+	"libnvidia-opticalflow.so",
 	"libnvidia-ptxjitcompiler.so.1",
+	"libnvidia-ptxjitcompiler.so",
 	"libnvoptix.so.1",
+	"libnvoptix.so",
 };
 
 static const char *cuda_candidate_paths[] = {
